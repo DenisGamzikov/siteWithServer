245598e1a1ccfc2d187ba625a9ff9649ac753da7
DenisGamzikov <132223000+DenisGamzikov@users.noreply.github.com>
2023-07-15 21:50:53 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const loaderEl = document.getElementById("loader");
+
+const spanEl = document.getElementById("counter");
 function loadingData() {
   loaderEl.classList.add("d-block");
   fetch("./cities.json")
@@ -9,63 +11,70 @@ function loadingData() {
       let citiesHtml = "";
       cities.forEach((city) => {
         citiesHtml += `
-    <tr>
-    <td> ${city.address}</td>
+    <tr class="active-tr">
+    <td class-"cities"> ${city.address}</td>
     <td class="population"> ${city.population}</td>
     <td class="age"> ${city.foundation_year}</td>
     </tr>`;
       });
       loaderEl.classList.add("d-none");
       tableEl.innerHTML = citiesHtml;
+      let quantity = document.getElementsByTagName("tr").length;
+      spanEl.innerText = quantity;
     });
 
   function getResponse(response) {
     return response.json();
   }
 }
-const rowElements = document.querySelectorAll("tbody tr");
+
 function filter(moreThan, lessThan) {
+  const rowElements = document.querySelectorAll("tbody tr");
   console.log(moreThan, lessThan);
   rowElements.forEach((rowEl) => {
-    rowEl.classList.add("d-none");
+    rowEl.classList.remove("active-tr");
+    rowEl.classList.remove("d-none");
     population = rowEl.querySelector(".population").innerText;
-
     if (population >= moreThan && population <= lessThan) {
-      rowEl.classList.remove("d-none");
+      rowEl.classList.add("active-tr");
     } else {
       rowEl.classList.add("d-none");
     }
+    let quantity = document.querySelectorAll("tr.active-tr").length;
+    spanEl.innerText = quantity;
   });
 }
 
 function filterOld(data) {
+  const rowElements = document.querySelectorAll("tbody tr");
   rowElements.forEach((rowEl) => {
     ageEl = rowEl.querySelector(".age");
-    rowElements.forEach((rowEl) => {
+    rowEl.classList.remove("active-tr");
+    rowEl.classList.remove("d-none");
+    if (ageEl.innerText <= data) {
+      rowEl.classList.add("active-tr");
+    } else {
       rowEl.classList.add("d-none");
-      if (age.innerText <= data) {
-        rowEl.classList.remove("d-none");
-      } else {
-        rowEl.classList.add("d-none");
-      }
-    });
+    }
+    let quantity = document.querySelectorAll("tr.active-tr").length;
+    spanEl.innerText = quantity;
   });
 }
+
 function filterYoung(data) {
+  const rowElements = document.querySelectorAll("tbody tr");
   rowElements.forEach((rowEl) => {
     ageEl = rowEl.querySelector(".age");
-    rowElements.forEach((rowEl) => {
+    rowEl.classList.remove("active-tr");
+    rowEl.classList.remove("d-none");
+    if (ageEl.innerText >= data) {
+      rowEl.classList.add("active-tr");
+    } else {
       rowEl.classList.add("d-none");
-      if (age.innerText >= data) {
-        rowEl.classList.remove("d-none");
-      } else {
-        rowEl.classList.add("d-none");
-      }
-    });
+    }
+    let quantity = document.querySelectorAll("tr.active-tr").length;
+    spanEl.innerText = quantity;
   });
 }
+
 loadingData();
-const tableEl = document.getElementById("table");
-const quantity = document.getElementsByTagName("tr").length;
-const spanEl = document.getElementById("counter");
-spanEl.innerText = quantity;
